78033b85178d47a985932a72ac35cfe8dc89e17e
Daniel Kendell <daniel_kendell@macdan.net>
2012-11-11 20:03:01 +0000

    Updated the Readme and license'd it up! There, it's done now. All formal'n'such...

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
 Push Me Pull You
 ================
 
-A Websocket/HTTP message bus
+A Websocket/HTTP message bus.
+
+Open WebSocket connections to any path to create a bus, messages sent to it will be relayed to all other WebSocket connections that were made to the same bus.
+
+An HTTP GET request on any path will retrieve the last message sent to that bus.
+
+An HTTP POST request on any path will push it's request body to the bus and it's subscribers.
+
+Instructions
+------------
+
+Currently the build is left to rebar so you'll have to have that installed.
+
+	rebar get-deps
+	rebar compile
+
+Starting Push Me Pull You is simple. 
+If you want to start it, run <code>./start.sh</code>. 
+You can work out the rest of the controls for yourself.
+
+License
+-------
+
+See COPYING.
--- src/pmpy.erl
+++ src/pmpy.erl
@@ -1,3 +1,20 @@
+% Copyright 2012 Daniel Kendell <daniel.kendell@gmail.com>
+
+% This file is part of PushMePullYou.
+%
+% PushMePullYou is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% PushMePullYou is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with PushMePullYou.  If not, see <http://www.gnu.org/licenses/>.
+
 -module( pmpy ).
 
 -export( [ start/0, stop/0, endpoint/1, subscribe/1, unsubscribe/1, notify/2, get_latest/1 ] ).
